99bd68103aaeea38b3ca144ab13bce4f2a155a3d
Rawdney Mendes <mendesrawdney@gmail.com>
2024-09-29 12:32:44 -0300

    feat(request): add userId property to Request interface fix(comments): correct typo in comment docs(README): update project setup instructions

--- backend/src/config/firebaseAdmin.ts
+++ backend/src/config/firebaseAdmin.ts
@@ -1,6 +1,6 @@
 import admin from 'firebase-admin';
 
-const serviceAccount = require('caminho/para/sua-chave.json'); 
+const serviceAccount = require('../config/stellar-arcadia-332916-firebase-adminsdk-tz6kz-f3ec67a494.json'); 
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
