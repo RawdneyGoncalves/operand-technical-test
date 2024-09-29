0c5ca676e4d569fdf489114d133c1ea2b9eca780
Rawdney Mendes <mendesrawdney@gmail.com>
2024-09-29 01:24:39 -0300

    refactor(routes): separate auth and task routes for better organization fix(AuthService): resolve token verification issue

--- backend/src/config/firebaseAdmin.ts
+++ backend/src/config/firebaseAdmin.ts
@@ -1,7 +1,6 @@
-// firebaseAdmin.ts
 import admin from 'firebase-admin';
 
-const serviceAccount = require('caminho/para/sua-chave.json'); // Substitua pelo caminho correto
+const serviceAccount = require('caminho/para/sua-chave.json'); 
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
--- backend/src/services/AuthService.ts
+++ backend/src/services/AuthService.ts
@@ -1,4 +1,5 @@
-import { auth } from '../config/firebase'; // Isso deve estar correto
+import { auth as clientAuth } from '../config/firebase'; 
+import { auth as adminAuth } from '../config/firebaseAdmin'; 
 import { signInWithEmailAndPassword, createUserWithEmailAndPassword, sendPasswordResetEmail } from 'firebase/auth'; 
 import { IAuthService } from '../interfaces/IAuthService';
 import { User } from '../models/User';
@@ -6,20 +7,25 @@ import { User } from '../models/User';
 export class AuthService implements IAuthService {
   async register(userData: Omit<User, "id">): Promise<User> {
     const { email, password } = userData;
-    const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+    const userCredential = await createUserWithEmailAndPassword(clientAuth, email, password);
     return { id: userCredential.user.uid, email: userCredential.user.email!, password, role: 'user' };
   }
 
   async login(email: string, password: string): Promise<string | null> {
-    const userCredential = await signInWithEmailAndPassword(auth, email, password);
+    const userCredential = await signInWithEmailAndPassword(clientAuth, email, password);
     return userCredential.user.uid; 
   }
 
   async resetPassword(email: string): Promise<void> {
-    await sendPasswordResetEmail(auth, email);
+    await sendPasswordResetEmail(clientAuth, email);
   }
 
   async verifyToken(token: string): Promise<any> {
-    // Implemente a lógica para verificar o token
+    try {
+      const decodedToken = await adminAuth.verifyIdToken(token);
+      return decodedToken; 
+    } catch (error) {
+      throw new Error('Token verification failed');
+    }
   }
 }
